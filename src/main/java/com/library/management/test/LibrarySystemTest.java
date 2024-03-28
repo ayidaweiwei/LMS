6fda9e3b43df19361f30d230ece528c89b35697c
ayidaweiwei <463334669@qq.com>
2024-03-28 08:05:08 +0800

    Add(LMS): update
    update the Test import lib

--- src/main/java/com/library/management/test/LibrarySystemTest.java
+++ src/main/java/com/library/management/test/LibrarySystemTest.java
@@ -9,7 +9,8 @@ import com.library.management.service.LibrarySystem;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class LibrarySystemTest {
     private LibrarySystem librarySystem;
